d97b09fd48f06c4b3a137769a5238558a68043c7
Richard Messing <rsmessing@gmail.com>
2016-11-15 01:36:54 -0500

    Validations for no radio button and/or no checkbox activations

--- app/controllers/handoffs_controller.rb
+++ app/controllers/handoffs_controller.rb
@@ -22,23 +22,27 @@ class HandoffsController < ApplicationController
   end
 
   def create
-    validate_check = false
-    params[:handoff].each do |handoff|
-      if params[:attend_type] == nil
-         flash[:danger] = "Select Drop-off or Pick-up."
-         redirect_to (:back)
-         return
-      end      
+    validate_check = 0
+    if params[:attend_type] == nil
+       flash[:danger] = "Select Drop-off or Pick-up."
+       redirect_to (:back)
+       return
+    end 
+    params[:handoff].each do |handoff|     
       if handoff[:check] == "1"
-         validate_check = true
+         validate_check += 1
          @attendance = Handoff.new(attend: params[:attend_type], child_id: handoff[:child_id], child_fname: handoff[:child_fname], child_mname: handoff[:child_mname], child_lname: handoff[:child_lname], center_id: handoff[:center_id], escort_fname: handoff[:escort_fname], escort_lname: handoff[:escort_lname], group_name: handoff[:group_name])
          @attendance.save
       end
     end
-    if !validate_check
+    if validate_check == 0
        flash[:danger] = "Select at least one child or logoff."
        redirect_to (:back)
        return
+    elsif validate_check == 1
+       flash[:info] = "#{validate_check} child was checked in."
+    else
+       flash[:info] = "#{validate_check} children were checked in."
     end
     redirect_to parent_log_in_path
   end
