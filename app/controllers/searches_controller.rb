0850a06f0fedb22c1dd564da70793e9206164c59
Richard Messing <rsmessing@gmail.com>
2017-03-19 19:59:21 -0400

    added column sort to report

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,5 @@
 class SearchesController < ApplicationController
+  helper_method :sort_column, :sort_direction
 
   def show
   
@@ -13,6 +14,7 @@ class SearchesController < ApplicationController
     @handoffs = @search.search_handoffs.where(:center_id => @center.id)
     # Filter attendance search by date range
     @handoffs = @handoffs.where('created_at > ? AND created_at < ?', @search.date_from.beginning_of_day, @search.date_to.end_of_day) if @search.date_from.present? && @search.date_to.present?
+    @handoffs = @handoffs.order(sort_column + " " + sort_direction)
   end
 
   def create
@@ -27,6 +29,14 @@ class SearchesController < ApplicationController
 
   def search_params
   	  params.require(:search).permit(:class_name, :child_name, :adult_name, :location_name, :date_from, :date_to)
+  end
+
+  def sort_column
+    @handoffs.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
 end
